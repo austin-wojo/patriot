ca190f0c91ca3ec09cf9aae58790a2e64f2c0f19
Austin Wojciechowski <awojo93@gmail.com>
2024-02-18 18:35:43 -0500

    web page first design

--- run.py
+++ run.py
@@ -4,3 +4,17 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    """
+    https://www.goody2.ai/
+    remove navbar
+        made it just show the links at top then \ as you scroll down
+    Clean hero with clear call to action
+        sign up / donate button?
+        borderless design 
+    Bold Leadership: A primary color scheme of deep red, navy blue, and white.
+    Heritage and Honor: A palette of burgundy red, gold, and dark gray.
+    Primary Colors: Bright red, slate gray, and white.
+
+    """
